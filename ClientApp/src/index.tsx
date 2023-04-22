5fb07736a7e2ee0311069acfe884bcd1f11abf4e
Johny Tomogavk <boss.vanya20@mail.ru>
2023-04-22 15:00:11 +0300

    - Update webpack config for scss modules; - Update file structure; - Update theme config for ant design; - Add dependency with icons from antd;

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -1,9 +1,25 @@
 import * as ReactDOMClient from 'react-dom/client';
 import React from 'react';
-import App from './components/app';
+import App from './components/app/App';
+import { ConfigProvider, theme } from 'antd';
 
 const root = ReactDOMClient.createRoot(
   document.querySelector('#root') as HTMLElement
 );
 
-root.render(<App />);
+const app: JSX.Element = (
+  <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        algorithm: [theme.defaultAlgorithm],
+        token: {
+          borderRadius: 0,
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
+
+root.render(app);
