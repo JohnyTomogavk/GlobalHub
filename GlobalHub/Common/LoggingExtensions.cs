eaae54866159ab7ef1b2d5c4ab27e7494a7ad20a
Johny Tomogavk <boss.vanya20@mail.ru>
2023-06-04 20:17:07 +0300

    - Add Elastic Search + Kibana + Serilog for logging storage and displaying; - Add docker images to docker-compose file.

---
+++ GlobalHub/Common/LoggingExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Common;
+
+public static class SerilogExtensions
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> LoggerConfiguration =>
+        (context, configuration) =>
+        {
+            var elasticUri = context.Configuration.GetValue<string>("LogStorageUri");
+
+            configuration
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(
+                    new ElasticsearchSinkOptions(new Uri(elasticUri))
+                    {
+                        IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+                        TemplateName = "Common service logs template",
+                        AutoRegisterTemplate = true,
+                        NumberOfShards = 2,
+                        NumberOfReplicas = 1,
+                        OverwriteTemplate = true,
+                        AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                        TypeName = null,
+                        BatchAction = ElasticOpType.Create
+                    })
+                .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                .Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
+                .ReadFrom.Configuration(context.Configuration);
+        };
+}
