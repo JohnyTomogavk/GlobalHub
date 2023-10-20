34059c776bffb6b628c0f9713ab746120ec9c0e4
Ivan Sachishin <boss.vanya20@mail.ru>
2023-10-21 00:22:27 +0300

    - Fix type in env variable name.

--- GlobalHub/NotesService/Constants/ConfigConstants.cs
+++ GlobalHub/NotesService/Constants/ConfigConstants.cs
@@ -2,7 +2,7 @@
 
 public static class ConfigConstants
 {
-    public const string NoteDbConnectionStringEnvKey = "NOTES_SERIVCE_STORAGE_CONNECTION_STRING";
+    public const string NoteDbConnectionStringEnvKey = "NOTES_SERVICE_STORAGE_CONNECTION_STRING";
     public const string NotesStorageDbName = "NotesStore";
     public const string NotesEntityCollectionName = "Notes";
 }
