72b203a2f16c0e94e4d0cc6a41b6ca4b79e6e842
Ivan Sachishin <vanyasachishin@gmail.com>
2023-11-10 03:47:36 +0300

    - Fix note data invalidation on update iwth empty block.

--- ClientApp/src/components/richTextEditor/RichTextEditor.tsx
+++ ClientApp/src/components/richTextEditor/RichTextEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useId, useRef } from 'react';
-import { OutputData } from '@editorjs/editorjs';
+import { LogLevels, OutputData } from '@editorjs/editorjs';
 import { editorTools } from '../../config/editorJsTools';
 import { StyledEditorJsWrapper } from './StyledEditorJsWrapper';
 import { observer } from 'mobx-react-lite';
@@ -7,6 +7,7 @@ import UiConfigStore from '../../store/uiConfigStore';
 import { theme } from 'antd';
 import { isEqual } from 'lodash';
 import Editor from '@stfy/react-editor.js';
+import { NOTE_DEFAULT_CONTENT, NOTE_DEFAULT_EMPTY_BLOCK } from '../../constants/notesConstants';
 
 interface EditorParameters {
   onChange: (data: OutputData) => Promise<void>;
@@ -37,14 +38,19 @@ export const RichTextEditor = observer((props: EditorParameters): JSX.Element =>
 
   const onEditorDataChange = async (data: OutputData): Promise<void> => {
     if (isEqual(data.blocks, props.data.blocks)) return;
-    await props.onChange(data);
+
+    const dataToUpdate = {
+      ...data,
+      blocks: data.blocks.length === 0 ? [JSON.parse(NOTE_DEFAULT_EMPTY_BLOCK)] : data.blocks,
+    } as OutputData;
+
+    await props.onChange(dataToUpdate);
   };
 
   return (
     <StyledEditorJsWrapper id={editorHolderId} $isDarkTheme={isDarkTheme} $textColor={colorText}>
       <Editor
         ref={editorRef}
-        data={props.data}
         reinitOnPropsChange={false}
         tools={editorTools}
         onReady={onEditorReady}
