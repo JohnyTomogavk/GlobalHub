9fff87cb26e5dc8e9fecae0efb72f17f8269f922
Ivan Sachishin <vanyasachishin@gmail.com>
2023-12-19 00:27:18 +0300

    - Fix build issue.

--- GlobalHub/Services/BudgetService/BudgetsService.Infrastructure/GlobalUsings.cs
+++ GlobalHub/Services/BudgetService/BudgetsService.Infrastructure/GlobalUsings.cs
@@ -1,7 +1,5 @@
 ﻿// Global using directives
 
-global using System.Security.Claims;
 global using BudgetsService.Infrastructure.Extensions;
 global using BudgetsService.Infrastructure.Models;
 global using BudgetsService.Infrastructure.Services.Interfaces;
-global using Microsoft.AspNetCore.Http;
